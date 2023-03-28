6306ff2ec9570ad737067a0a7fd84d7517e5a807
Harshit Parashar <harshit.parashar@clecotech.com>
2023-03-28 17:40:07 +0530

    Worked on Login and Signup with jwt

--- src/Components/Screens/Login.jsx
+++ src/Components/Screens/Login.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
     email: "",
     password: "",
   });
-
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch("http://localhost:5000/api/loginuser", {
@@ -22,8 +22,14 @@ const Login = () => {
     });
     const json = await response.json();
     console.log(json);
+
     if (!json.success) {
       alert("Enter valid credentials");
+    }
+    if (json.success) {
+      localStorage.setItem("authToken", json.authToken);
+      console.log(localStorage.getItem("authToken"))
+      navigate("/");
     }
   };
 
